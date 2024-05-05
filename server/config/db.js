64b0ba62b81f5b30ffd51d5af2840463e581e31e
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2024-05-05 08:05:34 +0530

    done

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require("dotenv").config({ path: "./config.env"});
 
 const connectDB = async () =>{
     return mongoose
-    .connect("mongodb+srv://pamithapankaja:pamitha7890@cluster0.1tmigvh.mongodb.net/ToDoApp")
-    .then(() => console.log(`connection to ddatabse established`))
+    .connect(process.env.MONGODB_URL || "mongodb+srv://pamithapankaja:pamitha7890@cluster0.1tmigvh.mongodb.net/ToDoApp")
+    .then(() => console.log(`connection to databse established`))
     .catch((err) => console.log(err))
 };
 
